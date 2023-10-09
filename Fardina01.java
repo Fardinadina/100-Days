b84ac945a974f43c244ef06375c2bebbd4876daa
Fardinadina <frafardina@gmail.com>
2023-10-09 18:13:38 +0700

    commit pertama

---
+++ Fardina01.java
@@ -0,0 +1,21 @@
+package fardina01;
+public class Fardina01 {
+    public static void main(String[] args) {
+        
+        String Umur = "18";
+        byte umur1 = '2';
+        short umur2 = 18;
+        int umur3 = 12;
+        long umur4 = 13;
+        float umur5 = 2.3f;
+        double umur6 = 12.3;
+        System.out.println("umur + umur1 + umur2 + umur 3 + umur4 + umur5 + umur6  ");
+        
+        
+        
+        
+        
+        
+          }
+    
+}
