993970139e2d48b723555469bab68e242426072f
Fardinadina <frafardina@gmail.com>
2023-10-10 17:03:14 +0700

    commit kedua

---
+++ Fardina02.java
@@ -0,0 +1,18 @@
+package fardina02;
+import java.util.Scanner;
+public class Fardina02 {
+        public static void main(String[] args) {
+           Scanner input = new Scanner(System.in);
+           int a,b,c;
+           System.out.print("masukkan a = ");
+           a = input.nextInt();
+           System.out.print("masukkan b = ");
+           b = input.nextInt();
+           c = a + b;
+            System.out.print(a + " + " + b +  " = ");
+            System.out.println(c);
+            
+            
+       }
+    
+}
