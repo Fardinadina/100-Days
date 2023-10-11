e8e0bbbca0ef01a04a5dffa8446225a1f475b460
Fardinadina <frafardina@gmail.com>
2023-10-11 09:29:36 +0700

    commit ketiga

---
+++ Persegi03.java
@@ -0,0 +1,24 @@
+package persegi03;
+import java.util.Scanner;
+public class Persegi03 {
+   public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   double sisi1,sisi2;
+       System.out.print("Masukkan sisi 1 : ");
+       sisi1 = input.nextDouble();
+       System.out.print("Masukkan sisi 2 : ");
+       sisi2 = input.nextDouble();
+       double luas = sisi1*sisi2;
+       double keliling = 4*sisi1;
+       System.out.println("luas: " + luas);
+       System.out.println("Keliling: " + keliling);
+       
+        
+        
+        
+        
+        
+        
+    }
+    
+}
