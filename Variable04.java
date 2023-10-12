b15a907ac33a27c45a2ceeeab717a1a881962c92
Fardinadina <frafardina@gmail.com>
2023-10-12 18:25:30 +0700

    commit keempat

---
+++ Variable04.java
@@ -0,0 +1,25 @@
+package variable04;
+public class Variable04 {
+    public static void main(String[] args) {
+       int umur = 18;
+        System.out.println("umur = " + umur + " tahun" );
+        float berat =  (float) 15.4;
+        System.out.println("berat = " + berat + " kg");
+        double tinggi = 14.3;
+        System.out.println("tinggi = " + tinggi + " m");
+        
+        String nama = "Fardina";
+        System.out.println("nama = " + nama);
+        char gender = 'P';
+        System.out.println("gender = " + gender);
+
+
+
+
+
+
+
+
+    }
+    
+}
