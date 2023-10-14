00c2d84e3cc46c1d9b6bb58c973beec2c3996ae6
Fardinadina <frafardina@gmail.com>
2023-10-14 12:39:40 +0700

    commit keenam

---
+++ Penugasann06.java
@@ -0,0 +1,29 @@
+package penugasann06;
+public class Penugasann06 {
+
+    public static void main(String[] args) {
+        /*
+        +=
+        -=
+        *=
+        /=
+        %=
+        */
+        int a,b;
+        a = 14;
+        b = 4;
+        System.out.println(a);
+        a -= b; 
+        System.out.println(a);
+        a += b;
+        System.out.println(a);
+        a *= b;
+        System.out.println(a);
+     
+        
+        
+        
+        
+    }
+    
+}
