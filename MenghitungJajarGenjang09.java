d6f8619731b420a9e325ee934a6a4d8682ddc8bc
Fardinadina <frafardina@gmail.com>
2023-10-17 21:15:37 +0700

    commit kesembilan

---
+++ MenghitungJajarGenjang09.java
@@ -0,0 +1,20 @@
+package menghitungjajargenjang09;
+import java.util.Scanner;
+public class MenghitungJajarGenjang09 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        int alas,tinggi;
+        System.out.print("masukkan alas: " );
+        alas = input.nextInt();
+        System.out.print("masukkan tinggi: ");
+        tinggi = input.nextInt();
+        int luas = alas*tinggi;
+        System.out.println("luas: " + luas);
+        
+        
+        
+        
+        
+    }
+    
+}
