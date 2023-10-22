a65b1cd0188be6047896d08decf81d5c69a0647c
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-10-22 22:38:46 +0800

    Create Menghutunghargabarangrumah14.java

---
+++ Menghutunghargabarangrumah14.java
@@ -0,0 +1,32 @@
+package hargabarangrumah;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double harga = 0,pilihan,unit;
+        String Barang = null;
+        System.out.println("Harga barang rumah");
+        System.out.println("===================");
+        System.out.println("1. spring bed\n2. boneka\n3. bantal");
+        System.out.print("Pilihan : ");
+        pilihan = input.nextDouble();
+        System.out.print("Masukkan Jumlah unit/barang : ");
+        unit = input.nextDouble();
+        
+        if (pilihan == 1){
+            Barang = "Spring bed";
+            harga = 2000*unit;
+        }else if (pilihan == 2){
+            Barang = "Boneka";
+            harga = 1000*unit;
+        }else if (pilihan == 3){
+            Barang = "bantal";
+            harga = 500*unit;
+            
+        }
+        System.out.println("Barang yang anda pilih : " + Barang);
+        System.out.println("Total harga : Rp. "+ harga);
+    }
+    
+}
