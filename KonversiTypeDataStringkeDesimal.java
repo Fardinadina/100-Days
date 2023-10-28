fa6c90b5d4a5f94ebe7e79495f210ab951b998f3
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-10-28 22:33:59 +0800

    Create KonversiTypeDataStringkeDesimal20.java

---
+++ KonversiTypeDataStringkeDesimal.java
@@ -0,0 +1,21 @@
+package BilanganDesimal;
+import java.math.BilanganDesimal;
+public class BilanganDesimal {
+     public static void main(String[] args) {
+         BigDecimal a,b,c,d,e,f,g,h;
+         a = new BigDecimal("10.1");
+         b = new BigDecimal("11.1");
+         c = new BigDecimal("22.1");
+         d = new BigDecimal("33.1");
+         
+         e = a.add(a); //penjumlahan
+         f = b.multiply(d);// perkalian 
+         g = c.subtract(d);//pengurangan
+         h = d.divide(d); // pembagian
+         
+         System.out.println(e+"\n"+f+"\n"+g+"\n"+h);
+   
+    }
+    
+  
+}
