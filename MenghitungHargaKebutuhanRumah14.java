d0a5bb55ada95cdc93cba4a501dfa98a741098cb
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-10-31 09:16:06 +0700

    Update MenghitungHargaKebutuhanRumah13.java

--- MenghitungHargaKebutuhanRumah14.java
+++ MenghitungHargaKebutuhanRumah14.java
@@ -1,28 +1,28 @@
-package hargabarangceue;
+package HargaKebutuhanRumah;
 import java.util.Scanner;
-public class Hargabarangceue {
+public class HargaKebutuhanRumah {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double harga = 0,pilihan,unit;
         String Barang = null;
-        System.out.println("Harga barang Ceue");
+        System.out.println("HargaKebutuhanRumah");
         System.out.println("===================");
-        System.out.println("1. spring bed\n2. boneka\n3. bantal");
+        System.out.println("1. kursi\n2. lemari\n3. vas bunga");
         System.out.print("Pilihan : ");
         pilihan = input.nextDouble();
         System.out.print("Masukkan Jumlah unit/barang : ");
         unit = input.nextDouble();
         
         if (pilihan == 1){
-            Barang = "Spring bed";
-            harga = 2000*unit;
+            Barang = "kursi";
+            harga = 35000*unit;
         }else if (pilihan == 2){
-            Barang = "Boneka";
-            harga = 1000*unit;
+            Barang = "lemari";
+            harga = 65000*unit;
         }else if (pilihan == 3){
-            Barang = "bantal";
-            harga = 500*unit;
+            Barang = "vas bunga";
+            harga = 20000*unit;
             
         }
         System.out.println("Barang yang anda pilih : " + Barang);
