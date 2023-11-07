2e8e176d255a06a13dbbe9f3ceab0abb4b5286c2
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-11-07 20:31:12 +0800

    Create PercabanganIfElse30.java

---
+++ PercabanganIfElse30.java
@@ -0,0 +1,13 @@
+package PercabanganIfElse30;
+public class Main {
+public static void main(String[ ] args) {
+double total = 100000;
+System.out.println("Belanja = Rp. "+total);
+if(total >= 100000) {
+System.out.println("Dapat diskon 10%");
+double diskon = 10 * total / 100;
+total -= diskon;
+}
+System.out.println("Bayaran Akhir = Rp. "+total);
+}
+}
