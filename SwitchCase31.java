e7f9a9515381ee13609fe4ca08b69be8f254f953
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-11-08 19:16:55 +0800

    Create SwitchCase31.java

---
+++ SwitchCase31.java
@@ -0,0 +1,16 @@
+package SwitchCase31;
+import java.util.Scanner;
+public class Main {
+public static void main(String[ ] args) {
+Scanner masuk = new Scanner(System.in);
+int pil = masuk.nextInt();
+switch(pil){
+   case 1:
+              System.out.println("Laki-laki");
+break;
+case 2:
+System.out.println("Perempuan");
+break;
+
+}
+}
