5a0d9fe07a808165dfb3ef50172eee8eca7e6cbd
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-11-11 23:28:59 +0800

    Create Mainjava34.java

---
+++ Maijava34.java
@@ -0,0 +1,20 @@
+package mainjava34;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Tabel Perkalian");
+        
+        System.out.print("Masukkan angka : ");
+        int angka = input.nextInt();
+        
+        System.out.println("Berikut adalah tabel perkalian " +angka);
+        
+        for(int i=0; i<10; i++){
+            System.out.println(angka +" x "+ (i+1) +" = "+ angka*(i+1));
+        }
+    }
+}
