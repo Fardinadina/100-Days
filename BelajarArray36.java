51dde5c4bc7d6919fbd5a9ad68586ac278a5c9c8
Fardina <147162916+Fardinadina@users.noreply.github.com>
2023-11-13 20:05:41 +0700

    Create BelajarArray36.java
    day36

---
+++ BelajarArray36.java
@@ -0,0 +1,18 @@
+package liriktanparasabersalah24;
+
+public class LirikTanpaRasaBersalah24 {
+    public static void main(String[] args) {
+        
+
+        
+        
+        int [] a = {3,5,7,9,11};
+        int jml = a.length;
+        
+        for (int i = 0; i<jml; i++){
+            System.out.print(a[i]+ " ");
+        }
+        System.out.println("");
+    }
+  
+}
